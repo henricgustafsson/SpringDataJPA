38d7bae019180035fb42a3b40fac18e2a413f688
henricgustafsson <50734521+henricgustafsson@users.noreply.github.com>
2020-01-22 14:56:15 +0100

    finally_got_application_properties_working_used_right_config_wrong_path_made_new_applicaion_properties_file_under_src_bc_im_dumb

--- src/main/java/se/lexicon/henric/SpringDataJPA/SpringDataJpa2Application.java
+++ src/main/java/se/lexicon/henric/SpringDataJPA/SpringDataJpa2Application.java
@@ -3,6 +3,7 @@ package se.lexicon.henric.SpringDataJPA;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringDataJpa2Application {
 
